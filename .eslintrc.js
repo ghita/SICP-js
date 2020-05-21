51b2426c21787d45f883412b4da7917157e6f9cb
Gheorghe Marinca <gheorghe.marinca@telenav.com>
2020-05-21 18:04:51 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {browser: false, es6: true},
+  globals: {Atomics: 'readonly', SharedArrayBuffer: 'readonly'},
+  parserOptions: {ecmaVersion: 11, sourceType: 'module'},
+  rules: {}
+}
